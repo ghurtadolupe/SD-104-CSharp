ca33df26176689ef0f85e07d097d73f6987cf5d4
Guadalupe Hurtado <hurtadoglupe@hotmail.com>
2019-08-05 14:50:40 -0500

    Homework 2

---
+++ HW 2/HW 2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HW_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declare variables
+            double dblFirstNumber;
+            double dblSecondNumber;
+            double dblThirdNumber;
+
+            //user input
+            Console.WriteLine("Enter your first number: ");
+            dblFirstNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter your second number: ");
+            dblSecondNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter your third number: ");
+            dblThirdNumber = Convert.ToInt32(Console.ReadLine());
+
+        }
+    }
+}
