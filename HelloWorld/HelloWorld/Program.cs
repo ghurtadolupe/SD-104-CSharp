aa3a17eb07065baa77377a6c6b6fd69a4cb9ff08
Guadalupe Hurtado <hurtadoglupe@hotmail.com>
2019-08-05 09:18:27 -0500

    First Commit Hello World

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I Am Here!");
+            Console.ReadLine();
+        }
+    }
+
