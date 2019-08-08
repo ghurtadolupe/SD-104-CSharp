76233708b9629cc67af91190f1350a127a78cc22
Guadalupe Hurtado <hurtadoglupe@hotmail.com>
2019-08-08 11:33:37 -0500

    Lab cuatro

---
+++ Lab4/Lab4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declarations
+            int nLargest;
+            int nSmallest;
+            int nCurrent:
+            string strEntry;
+
+            //first integer
+            Console.WriteLine("Enter an integer ");
+            nLargest = nSmallest = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter another integer or Q to Quit");
+            strEntry = Console.ReadLine();
+
+            //loop
+            while(strEntry != "Q" )
+            {
+                nCurrent = Convert.ToInt32(strEntry);
+
+                Console.WriteLine("Enter another integer or Q to Quit");
+                strEntry = Console.ReadLine();
+            }
+
+
+
+        }
+    }
+}
