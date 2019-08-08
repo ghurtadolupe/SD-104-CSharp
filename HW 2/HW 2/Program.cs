2a10db26e3b2d0b104df80a2ec30f63c8ad52668
Guadalupe Hurtado <hurtadoglupe@hotmail.com>
2019-08-08 11:10:15 -0500

    HW2 finished

--- HW 2/HW 2/Program.cs
+++ HW 2/HW 2/Program.cs
@@ -18,8 +18,62 @@ namespace HW_2
             Console.WriteLine("Enter your second number: ");
             dblSecondNumber = Convert.ToInt32(Console.ReadLine());
 
+            //Finding the highest number from the two
+            if (dblFirstNumber > dblSecondNumber)
+            {
+                Console.WriteLine("Your highest number is " + dblFirstNumber);
+
+            }
+            else if (dblFirstNumber < dblSecondNumber)
+            {
+                Console.WriteLine("Your highest number is " + dblSecondNumber);
+            }
+
+
             Console.WriteLine("Enter your third number: ");
             dblThirdNumber = Convert.ToInt32(Console.ReadLine());
+
+           //Finding the highest number from the three
+
+            if (dblThirdNumber > dblFirstNumber && dblThirdNumber > dblSecondNumber)
+            {
+                Console.WriteLine("Your biggest number is actually "+ dblThirdNumber);
+            }
+
+            else if(dblSecondNumber > dblFirstNumber && dblSecondNumber > dblThirdNumber)
+            {
+                Console.WriteLine("Your biggest number is actually " + dblSecondNumber);
+            }
+
+            else if (dblFirstNumber > dblSecondNumber && dblFirstNumber > dblThirdNumber)
+            {
+                Console.WriteLine("Your biggest number is actually " + dblFirstNumber);
+            }
+
+
+            //Finding the lowest number
+
+            if (dblFirstNumber < dblSecondNumber && dblFirstNumber < dblThirdNumber)
+            {
+                Console.WriteLine("The lowest number was " + dblFirstNumber);
+            }
+
+            else if (dblSecondNumber < dblFirstNumber && dblSecondNumber < dblThirdNumber)
+            {
+                Console.WriteLine("The lowest number was " + dblSecondNumber);
+            }
+
+            else if (dblThirdNumber < dblFirstNumber && dblThirdNumber < dblSecondNumber)
+            {
+                Console.WriteLine("The lowest number was " + dblThirdNumber);
+            }
+
+
+
+
+
+            Console.ReadLine();
+
 
         }
     }
