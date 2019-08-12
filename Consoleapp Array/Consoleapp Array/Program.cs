00a2ba3f9ec5b033b7fc81374d09a17ccb2e7855
Guadalupe Hurtado <hurtadoglupe@hotmail.com>
2019-08-12 11:10:04 -0500

    Notes arrays

--- Consoleapp Array/Consoleapp Array/Program.cs
+++ Consoleapp Array/Consoleapp Array/Program.cs
@@ -8,7 +8,27 @@ namespace Consoleapp_Array
         {
 
             string[] DaysOfTheWeek = { "SAT", "SUN", "MON", "TUES", "WED", "THURS", "FRI" };
+            //The Length measures how many objects are inside the string
             Console.WriteLine(DaysOfTheWeek. Length. ToString());
+    
+            
+    /*
+           foreach (string day in DaysOfTheWeek)
+            {
+                Console.WriteLine("The day is : " + day);
+            }
+
+    */
+            //the top "foreach" loop is the same as this "for" loop 
+           
+            //First Declaring the variables (int i = 0;)
+            //While "i" is < DaysOfTheWeek's Length, execute this
+            //"i++" is saying continue adding one to "i"
+           for (int i = 0; i < DaysOfTheWeek.Length; i++)
+            {
+                Console.WriteLine("The day is : " + DaysOfTheWeek[i]);
+            }
+
 
             Console.ReadLine();
 
