36df4ceac531c6f4d7c37e9ac9db4c5c4972a495
Guadalupe Hurtado <hurtadoglupe@hotmail.com>
2019-08-12 14:41:38 -0500

    Finished guessing game

---
+++ Lab 4 Guess my number/Lab 4 Guess my number/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Lab_4_Guess_my_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            do
+            {
+
+                Random rand = new Random();
+                int hidden = rand.Next(100);
+                int guess = 0;
+                int guesses = 0;
+
+                //ask user for a number
+                Console.WriteLine("Guess a number between 0 and 100!");
+
+                while (guess != hidden)
+                {
+                    guesses++;
+
+                    int.TryParse(Console.ReadLine(), out guess);
+
+
+                    //keep guessing until they guess the number
+                    if (guess > hidden)
+                    {
+                        Console.WriteLine("Nope, " + guess + " is too high.");
+                    }
+
+                    else if (guess < hidden)
+                    {
+                        Console.WriteLine("No no no, " + guess + " is too low.");
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("That is correct!! " + guess + " Is the correct answer! You Win Nothing!");
+                        Console.WriteLine("Took you long enough! Thankyou for wasting my RAM");
+                        Console.WriteLine("Number of guesses: " + guesses);
+
+                    }
+
+                }
+                Console.WriteLine("Would you like to play again? Type Y for Yes or any other key to quit");
+            } while (Console.ReadLine() == "Y");
+
+           
+
+        }
+    }
+}
--- Lab 6 Leap Year/Lab 6 Leap Year/Program.cs
+++ Lab 6 Leap Year/Lab 6 Leap Year/Program.cs
@@ -6,35 +6,37 @@ namespace Lab_6_Leap_Year
     {
         static void Main(string[] args)
         {
-            //Declarations
-            int nYear;
-            bool isDiv4;
-            bool isDiv100;
-            bool isDiv400;
-
-            //validate year input
             do
             {
-                Console.WriteLine("Please Enter a Year to see if it was or will be a leap year: ");
-            } while (!int.TryParse(Console.ReadLine(), out nYear));
+                //Declarations
+                int nYear;
+                bool isDiv4;
+                bool isDiv100;
+                bool isDiv400;
 
-            //calculate factors
-            isDiv4 = nYear % 4 == 0 ? true : false;
-            isDiv100 = nYear % 100 == 0 ? true : false;
-            isDiv400 = nYear % 400 == 0 ? true : false;
+                //validate year input
+                do
+                {
+                    Console.WriteLine("Please Enter a Year to see if it was or will be a leap year: ");
+                } while (!int.TryParse(Console.ReadLine(), out nYear));
 
-            //compound test to see if nYear is a leap year
-            if((isDiv4 && isDiv100 && isDiv400) || (isDiv4 && !isDiv100 && !isDiv400))
-            {
-                Console.WriteLine(nYear + "is a leap year.");
-            }
-            else
-            {
-                Console.WriteLine(nYear + " is not a leap year.");
-            }
+                //calculate factors
+                isDiv4 = nYear % 4 == 0 ? true : false;
+                isDiv100 = nYear % 100 == 0 ? true : false;
+                isDiv400 = nYear % 400 == 0 ? true : false;
 
-            Console.WriteLine("Would you like to test another year? Type Y for Yes or any other key to quit");
-            while (Console.ReadLine() == "Y") ;
+                //compound test to see if nYear is a leap year
+                if ((isDiv4 && isDiv100 && isDiv400) || (isDiv4 && !isDiv100 && !isDiv400))
+                {
+                    Console.WriteLine(nYear + " is a leap year.");
+                }
+                else
+                {
+                    Console.WriteLine(nYear + " is not a leap year.");
+                }
+
+                Console.WriteLine("Would you like to test another year? Type Y for Yes or any other key to quit");
+            } while (Console.ReadLine() == "Y");
         } 
     }
 }
