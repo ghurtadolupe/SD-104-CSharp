342ffa16ae3a42d1c9d5ecc9b4fe9ab46595a663
Guadalupe Hurtado <hurtadoglupe@hotmail.com>
2019-08-12 11:49:12 -0500

    Leap Year

---
+++ Lab 6 Leap Year/Lab 6 Leap Year/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Lab_6_Leap_Year
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declarations
+            int nYear;
+            bool isDiv4;
+            bool isDiv100;
+            bool isDiv400;
+
+            //validate year input
+            do
+            {
+                Console.WriteLine("Please Enter a Year to see if it was or will be a leap year: ");
+            } while (!int.TryParse(Console.ReadLine(), out nYear));
+
+            //calculate factors
+            isDiv4 = nYear % 4 == 0 ? true : false;
+            isDiv100 = nYear % 100 == 0 ? true : false;
+            isDiv400 = nYear % 400 == 0 ? true : false;
+
+            //compound test to see if nYear is a leap year
+            if((isDiv4 && isDiv100 && isDiv400) || (isDiv4 && !isDiv100 && !isDiv400))
+            {
+                Console.WriteLine(nYear + "is a leap year.");
+            }
+            else
+            {
+                Console.WriteLine(nYear + " is not a leap year.");
+            }
+
+            Console.WriteLine("Would you like to test another year? Type Y for Yes or any other key to quit");
+            while (Console.ReadLine() == "Y") ;
+        } 
+    }
+}
